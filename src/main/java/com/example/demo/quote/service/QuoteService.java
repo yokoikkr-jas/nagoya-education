cb44aa490d40af1498612c5397ae4726f0fd58b1
ryuma suzuki <suzukrym@jastec.co.jp>
2025-05-23 16:54:49 +0900

    add db export

--- src/main/java/com/example/demo/quote/service/QuoteService.java
+++ src/main/java/com/example/demo/quote/service/QuoteService.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.demo.quote.repository.QuoteRepository;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.util.*;
 
 /**
@@ -45,11 +47,40 @@ public class QuoteService {
      * 
      * @param quote 登録する名言オブジェクト
      * @return 実際に登録した名言オブジェクト
+     * 
+     * @author
+     * @since 2025/05/23
+     * 
+     * @param
+     * @return
+     * @throw
      */
     public Quote addQuote(Quote quote) {
         try {
             Quote savedQuote = quoteRepository.save(quote);
             // 課題2 登録時の外部ファイル書き込み
+            String text = savedQuote.getText();
+            String author = savedQuote.getAuthor();
+            String fileName = "src\\main\\java\\com\\example\\demo\\quote\\Quote.csv";
+            // text：名言オブジェクトに格納されている名言
+            // author：名言オブジェクトに格納されている著者
+            // fileName：外部ファイルの相対パス
+            FileWriter file = new FileWriter(fileName, true);
+            BufferedWriter writer = new BufferedWriter(file);
+
+            try {
+                // 外部ファイルへの書き込み
+                writer.write(text);
+                writer.write(",");
+                writer.write(author);
+                writer.newLine();
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            } finally {
+                writer.close();
+            }
+
             return savedQuote;
         } catch (Exception e) {
             e.printStackTrace();
