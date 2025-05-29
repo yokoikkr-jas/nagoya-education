ad3eea8b195ecb59cd72678d563a97013b1abc5b
rina hirano <hiranrin@jastec.co.jp>
2025-05-29 14:00:59 +0900

    test1

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,11 +3,38 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
+// 追加コード
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+
 @SpringBootTest
 class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// Mockオブジェクトの作成
+		QuoteRepository mockRepo = mock(QuoteRepository.class);
+
+		// モックのリストを作成
+		List<Quote> mockQuotes = Arrays.asList(new Quote("A"), new Quote("B"), new Quote("C"));
+
+		// モックの動作を定義
+		when(mockRepo.findAll()).thenReturn(mockQuotes);
+
+		// QuoteServiceのインスタンスを作成
+		QuoteService quoteService = new QuoteService(mockRepo);
+
+		// メソッドの実行
+		int count = quoteService.countQuotes();
+
+		// アサーション（期待値との比較）
+		assertEquals(3, count);
 	}
 
 }
