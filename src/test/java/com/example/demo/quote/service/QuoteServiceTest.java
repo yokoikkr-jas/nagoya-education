b9e3810c97b9ff89c56249e84f9ccde43cf4d39b
rina hirano <hiranrin@jastec.co.jp>
2025-06-02 17:56:12 +0900

    test_review3

--- src/test/java/com/example/demo/quote/service/QuoteServiceTest.java
+++ src/test/java/com/example/demo/quote/service/QuoteServiceTest.java
@@ -2,12 +2,12 @@ package com.example.demo.quote.service;
 
 // テストメソッドの定義に使う
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 
 // アサーションのインポート：実際の結果と期待する結果を比較
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-// Mockito(mock,when)をインポート：QuoteRepositoryのモックを作成し、テスト環境での動作を定義
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 // @Autowired：Springが自動でインスタンスを注入
@@ -18,15 +18,25 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import com.example.demo.quote.repository.QuoteRepository;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
 
 
 @SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class QuoteServiceTest {
     @Autowired
-    private QuoteService quoteService;// テストしたいメソッドがあるクラスを書く
+    private QuoteService quoteService;
 
     @Autowired
     private QuoteRepository quoteRepository;
+
+    @Mock
+    private QuoteRepository mockRepository;
+
+    @InjectMocks
+    private QuoteService mockService;
 
 
 
@@ -45,21 +55,17 @@ public class QuoteServiceTest {
         // DBに登録された全てのオブジェクトを削除
         quoteRepository.deleteAll();
 
-        // メソッドの実行
         int count = quoteService.countQuotes();
-
-        // アサーション（期待値との比較）
         assertEquals(0, count);
     }
 
 
     @Test // 例外処理（DBからQuoteオブジェクトを取得できなかったとき）
     public void testCountQuotesError() {
-        QuoteRepository mockRepo = mock(QuoteRepository.class);
-        when(mockRepo.findAll()).thenThrow(new RuntimeException("DB接続エラー"));
-        QuoteService quoteService = new QuoteService(mockRepo);
+        // mockの動作を定義
+        when(mockRepository.findAll()).thenThrow(new RuntimeException("DB接続エラー"));
 
-        int result = quoteService.countQuotes();
+        int result = mockService.countQuotes();
         assertEquals(0, result);
     }
 }
