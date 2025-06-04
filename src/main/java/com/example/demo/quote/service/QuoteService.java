a72bc42c6988ef349a5291c1625425c5cc0c7c78
rina hirano <hiranrin@jastec.co.jp>
2025-06-04 09:18:42 +0900

    test8

--- src/main/java/com/example/demo/quote/service/QuoteService.java
+++ src/main/java/com/example/demo/quote/service/QuoteService.java
@@ -19,10 +19,6 @@ public class QuoteService {
     @Autowired
     private QuoteRepository quoteRepository;
 
-    public QuoteService(QuoteRepository mockRepository) {
-        // TODO Auto-generated constructor stub
-    }
-
     /**
      * 名言一覧取得メソッド
      * 
