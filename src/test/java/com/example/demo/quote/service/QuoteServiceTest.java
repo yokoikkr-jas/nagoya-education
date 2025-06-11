a55d60bfd84e031a5b1d0cdca411b15ea98a5178
kakeru yokoi <yokoikkr@jastec.co.jp>
2025-06-11 17:13:57 +0900

    refactor

--- src/test/java/com/example/demo/quote/service/QuoteServiceTest.java
+++ src/test/java/com/example/demo/quote/service/QuoteServiceTest.java
@@ -11,7 +11,7 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
