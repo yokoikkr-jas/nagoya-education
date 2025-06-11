f984a073f86d55bb876299d80e0fdfeff44f11bd
ootayuuk-jas <ootayuuk@jastec.co.jp>
2025-06-11 16:33:54 +0900

    Add Feature/search quotes (#1)
    * add partialmatch
    
    * add partialmatch completion
    
    * add partialmatch textchange
    
    * add partialmatch textdelete
    
    * add partialmatch method authorname
    
    * add partialmatch testcode
    
    * add partialmatch fix
    
    * add partialmatch coment fix
    
    * resolve partialmatch review
    
    * refactor
    
    * add partialmatch namefix
    
    ---------
    
    Co-authored-by: kakeru yokoi <yokoikkr@jastec.co.jp>

--- src/main/java/com/example/demo/quote/service/QuoteService.java
+++ src/main/java/com/example/demo/quote/service/QuoteService.java
@@ -122,4 +122,56 @@ public class QuoteService {
             return null;
         }
     }
+
+    /**
+     * 部分一致検索を行うメソッド
+     * 
+     * @author 太田
+     * @since 2025/05/26
+     * 
+     * @param searchString
+     * @param subject
+     * @return 指定された文字列と部分一致する名言オブジェクトのリスト
+     */
+    public List<Quote> partialMatch(String searchString, String subject) {
+        try {
+            List<Quote> list = new ArrayList<>(); // 部分一致する名言オブジェクトの格納先
+            switch (subject) { // optionによる分岐（名言のみ、著者、両方）
+                case "text":
+                    for (Quote quotes : getAllQuotes()) { // DB上リストのサイズまで繰り返す
+                        if (quotes.getText().contains(searchString)) { // 部分一致:true
+                            list.add(quotes);
+                        }
+                    }
+                    break;
+                case "author":
+                    for (Quote quotes : getAllQuotes()) {
+                        if (quotes.getAuthor().contains(searchString)) {
+                            list.add(quotes);
+                        }
+                    }
+                    break;
+                case "both":
+                    for (Quote quotes : getAllQuotes()) {
+                        if (quotes.getText().contains(searchString)) {
+                            list.add(quotes);
+                        } else if (quotes.getAuthor().contains(searchString)) {
+                            list.add(quotes);
+                        }
+                    }
+                    break;
+            }
+
+            if (list.isEmpty()) { // 部分一致する文字列のリストbが空の場合、nullを返却
+                return null;
+            }
+
+            return list;
+
+        } catch (Exception e) {
+            e.printStackTrace(); // スタックトレースを出力する
+            return null;
+        }
+    }
+
 }
--- src/test/java/com/example/demo/quote/service/QuoteServiceTest.java
+++ src/test/java/com/example/demo/quote/service/QuoteServiceTest.java
@@ -2,22 +2,34 @@ package com.example.demo.quote.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.Mockito.doReturn;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
 
 import com.example.demo.quote.model.Quote;
+import com.example.demo.quote.repository.QuoteRepository;
 
 @SpringBootTest
 public class QuoteServiceTest {
+
+    @Mock
+    private QuoteRepository quoteRepositoryMock;
+
+    @InjectMocks
+    private QuoteService injectQuoteService;
 
     @MockitoSpyBean
     private QuoteService quoteService;
@@ -27,14 +39,11 @@ public class QuoteServiceTest {
      */
     @Test
     void testAddQuote1(@TempDir Path tempDir) throws Exception {
-        List<String> initialLines = List.of(
-                "quote,author");
+        List<String> initialLines = List.of("quote,author");
         Path csvFile = Files.write //
         (Files.createTempFile(tempDir, "sample1-", ".csv"), initialLines);
 
-        List<String> expectedList = List.of(
-                "quote,author",
-                "text1,author1");
+        List<String> expectedList = List.of("quote,author", "text1,author1");
 
         String csvFilePath = csvFile.toString();
 
@@ -52,19 +61,13 @@ public class QuoteServiceTest {
      */
     @Test
     void testAddQuote2(@TempDir Path tempDir) throws Exception {
-        List<String> initialLines = List.of(
-                "quote,author",
-                "text1,author1",
-                "text2,author2");
+        List<String> initialLines = List.of("quote,author", "text1,author1", "text2,author2");
 
         Path csvFile = Files.write //
         (Files.createTempFile(tempDir, "sample2-", ".csv"), initialLines);
 
-        List<String> expectedList = List.of(
-                "quote,author",
-                "text1,author1",
-                "text2,author2",
-                "text3,author3");
+        List<String> expectedList =
+                List.of("quote,author", "text1,author1", "text2,author2", "text3,author3");
 
         String csvFilePath = csvFile.toString();
 
@@ -82,18 +85,13 @@ public class QuoteServiceTest {
      */
     @Test
     void testAddQuote3(@TempDir Path tempDir) throws Exception {
-        List<String> initialLines = List.of(
-                "quote,author",
-                "text1,author1");
+        List<String> initialLines = List.of("quote,author", "text1,author1");
 
         Path csvFile = Files.write //
         (Files.createTempFile(tempDir, "sample3-", ".csv"), initialLines);
 
-        List<String> expectedList = List.of(
-                "quote,author",
-                "text1,author1",
-                "text2,author2",
-                "text3,author3");
+        List<String> expectedList =
+                List.of("quote,author", "text1,author1", "text2,author2", "text3,author3");
 
         String csvFilePath = csvFile.toString();
 
@@ -113,9 +111,7 @@ public class QuoteServiceTest {
      */
     @Test
     void testAddQuote4(@TempDir Path tempDir) throws Exception {
-        List<String> initialLines = List.of(
-                "quote,author",
-                "text1,author1");
+        List<String> initialLines = List.of("quote,author", "text1,author1");
 
         Path csvFile = Files.write //
         (Files.createTempFile(tempDir, "sample4-", ".csv"), initialLines);
@@ -132,20 +128,16 @@ public class QuoteServiceTest {
     }
 
     /*
-     * 出力する外部ファイルが存在しない場合、ファイルを自動的に作成しデータを出力する
-     * FileWriterの仕様上、出力先のファイルが自動生成される
+     * 出力する外部ファイルが存在しない場合、ファイルを自動的に作成しデータを出力する FileWriterの仕様上、出力先のファイルが自動生成される
      */
     @Test
     void testAddQuote5(@TempDir Path tempDir) throws Exception {
-        List<String> initialLines = List.of(
-                "quote,author",
-                "text1,author1");
+        List<String> initialLines = List.of("quote,author", "text1,author1");
 
         Path csvFile = Files.write //
         (Files.createTempFile(tempDir, "sample5-", ".csv"), initialLines);
 
-        List<String> expectedList = List.of(
-                "text2,author2");
+        List<String> expectedList = List.of("text2,author2");
 
         String csvFilePath = csvFile.toString();
         Files.delete(csvFile);
@@ -161,14 +153,11 @@ public class QuoteServiceTest {
     }
 
     /*
-     * 出力する外部ファイルが存在しない、かつ、ファイルを作成できない(ディレクトリを削除して状況を再現)場合、nullを返却する
-     * 外側のtry-catch、ファイル読み込み時の例外
+     * 出力する外部ファイルが存在しない、かつ、ファイルを作成できない(ディレクトリを削除して状況を再現)場合、nullを返却する 外側のtry-catch、ファイル読み込み時の例外
      */
     @Test
     void testAddQuote6(@TempDir Path tempDir) throws Exception {
-        List<String> initialLines = List.of(
-                "quote,author",
-                "text1,author1");
+        List<String> initialLines = List.of("quote,author", "text1,author1");
         Path csvFile = Files.write //
         (Files.createTempFile(tempDir, "sample6-", ".csv"), initialLines);
 
@@ -187,8 +176,7 @@ public class QuoteServiceTest {
     }
 
     /*
-     * 出力するファイルが通常ファイルとして存在していない(ディレクトリとして存在している)場合、nullを返却する
-     * 外側のtry-catch、ファイル読み込み時の例外
+     * 出力するファイルが通常ファイルとして存在していない(ディレクトリとして存在している)場合、nullを返却する 外側のtry-catch、ファイル読み込み時の例外
      */
     @Test
     void testAddQuote7(@TempDir Path tempDir) throws Exception {
@@ -203,14 +191,11 @@ public class QuoteServiceTest {
     }
 
     /*
-     * 出力する外部ファイルに出力できない場合、nullを返却する
-     * 内側のtry-catch、ファイル書き込み時の例外
+     * 出力する外部ファイルに出力できない場合、nullを返却する 内側のtry-catch、ファイル書き込み時の例外
      */
     @Test
     void testAddQuote8(@TempDir Path tempDir) throws Exception {
-        List<String> initialLines = List.of(
-                "quote,author",
-                "text1,author1");
+        List<String> initialLines = List.of("quote,author", "text1,author1");
         Path csvFile = Files.write //
         (Files.createTempFile(tempDir, "sample8-", ".csv"), initialLines);
 
@@ -225,4 +210,315 @@ public class QuoteServiceTest {
         assertNull(actualQuote);
     }
 
+    /*
+     * 前提条件：DBから名言オブジェクトを取得 手順：キーワードqueryと検索対象optionの条件で検索を行う。 入力値：query"地球"、option"text"
+     * 期待する結果：text"地球は青かった", author"ガガーリン"
+     * 
+     * 内容：検索対象が名言のみの場合、部分一致した名言オブジェクトの リストを作成し、そのリストを取り出せるかどうか。
+     */
+    @Test
+    void testPartialMatch_main() {
+        List<Quote> allList = new ArrayList<>();
+        List<Quote> actualList = new ArrayList<>();
+        List<Quote> expectedList = new ArrayList<>();
+        allList.add(new Quote("地球は青かった", "ガガーリン"));
+        allList.add(new Quote("天才は1%のひらめきと99%の努力でつくられる", "トーマス・エジソン"));
+        allList.add(new Quote("憧れるのをやめましょう", "大谷翔平"));
+        allList.add((new Quote("僕の大冒険", "僕")));
+        Mockito.when(quoteRepositoryMock.findAll()).thenReturn(allList);
+
+        actualList.addAll(injectQuoteService.partialMatch("地球", "text"));
+        expectedList.add(allList.get(0));
+        assertIterableEquals(expectedList, actualList);
+
+    }
+
+    /*
+     * 前提条件：DBから名言オブジェクトを取得 手順：キーワードqueryと検索対象optionの条件で検索を行う。 入力値：query"テスト", option"text"
+     * 期待する結果：null
+     * 
+     * 内容：検索対象が名言のみの場合、部分一致した名言オブジェクトの リストが空っぽの際にnull。
+     */
+    @Test
+    void testPartialMatchText_quoteEmpty() {
+        List<Quote> allList = new ArrayList<>();
+        allList.add(new Quote("地球は青かった", "ガガーリン"));
+        allList.add(new Quote("天才は1%のひらめきと99%の努力でつくられる", "トーマス・エジソン"));
+        allList.add(new Quote("憧れるのをやめましょう", "大谷翔平"));
+        allList.add((new Quote("僕の大冒険", "僕")));
+
+        Mockito.when(quoteRepositoryMock.findAll()).thenReturn(allList);
+
+        assertNull(injectQuoteService.partialMatch("テスト", "text"));
+    }
+
+    /*
+     * 前提条件：DBから名言オブジェクトを取得 手順：キーワードqueryと検索対象optionの条件で検索を行う。 入力値：query"テスト", option"text"
+     * 期待する結果：null
+     * 
+     * 内容：DBから受け取ったリストが空っぽの場合はnull。
+     */
+    @Test
+    void testPartialMatchText_dbEmpty() {
+        List<Quote> allList = new ArrayList<>();
+
+        Mockito.when(quoteRepositoryMock.findAll()).thenReturn(allList);
+
+        assertNull(injectQuoteService.partialMatch("テスト", "text"));
+
+    }
+
+    /*
+     * 前提条件：DBから名言オブジェクトを取得 手順：キーワードqueryと検索対象optionの条件で検索を行う。 入力値：query"は", option"text"
+     * 期待する結果："地球は青かった", "ガガーリン" "天才は1%のひらめきと99%の努力でつくられる", "トーマス・エジソン"
+     * 
+     * 内容：リスト内の要素位置からループの１回目と２回目を確認し、ループの開始地点と 終了条件に誤りがないか。
+     */
+    @Test
+    void testPartialMatchText_roop() {
+        List<Quote> allList = new ArrayList<>();
+        List<Quote> actuaList = new ArrayList<>();
+        List<Quote> expectedList = new ArrayList<>();
+        allList.add(new Quote("地球は青かった", "ガガーリン"));
+        allList.add(new Quote("天才は1%のひらめきと99%の努力でつくられる", "トーマス・エジソン"));
+        allList.add(new Quote("憧れるのをやめましょう", "大谷翔平"));
+        allList.add((new Quote("僕の大冒険", "僕")));
+
+        Mockito.when(quoteRepositoryMock.findAll()).thenReturn(allList);
+
+        actuaList.addAll(injectQuoteService.partialMatch("は", "text"));
+        expectedList.add(allList.get(0));
+        expectedList.add(allList.get(1));
+        assertIterableEquals(expectedList, actuaList);
+    }
+
+    /*
+     * 前提条件：DBから名言オブジェクトを取得 手順：キーワードqueryと検索対象optionの条件で検索を行う。 入力値：query"大"、option"author"
+     * 期待する結果：text"憧れるのをやめましょう", author"大谷翔平"
+     * 
+     * 内容：検索対象が著者の場合、部分一致した名言オブジェクトの リストを作成し、そのリストを取り出せるかどうか。
+     */
+    @Test
+    void testPartialMatchAuthor_main() {
+        List<Quote> allList = new ArrayList<>();
+        List<Quote> actuaList = new ArrayList<>();
+        List<Quote> expectedList = new ArrayList<>();
+        allList.add(new Quote("地球は青かった", "ガガーリン"));
+        allList.add(new Quote("天才は1%のひらめきと99%の努力でつくられる", "トーマス・エジソン"));
+        allList.add(new Quote("憧れるのをやめましょう", "大谷翔平"));
+        allList.add((new Quote("僕の大冒険", "僕")));
+
+        Mockito.when(quoteRepositoryMock.findAll()).thenReturn(allList);
+
+        actuaList.addAll(injectQuoteService.partialMatch("大", "author"));
+        expectedList.add(allList.get(2));
+        assertIterableEquals(expectedList, actuaList);
+
+    }
+
+    /*
+     * 前提条件：DBから名言オブジェクトを取得 手順：キーワードqueryと検索対象optionの条件で検索を行う。 入力値：query"ノーネイム", option"author"
+     * 期待する結果：null
+     * 
+     * 内容：検索対象が著者の場合、部分一致した名言オブジェクトの リストが空っぽの際にnull。
+     */
+    @Test
+    void testPartialMatchAuthor_quoteEmpty() {
+        List<Quote> allList = new ArrayList<>();
+        allList.add(new Quote("地球は青かった", "ガガーリン"));
+        allList.add(new Quote("天才は1%のひらめきと99%の努力でつくられる", "トーマス・エジソン"));
+        allList.add(new Quote("憧れるのをやめましょう", "大谷翔平"));
+        allList.add((new Quote("僕の大冒険", "僕")));
+
+        Mockito.when(quoteRepositoryMock.findAll()).thenReturn(allList);
+
+        assertNull(injectQuoteService.partialMatch("ノーネイム", "author"));
+
+    }
+
+    /*
+     * 前提条件：DBから名言オブジェクトを取得 手順：キーワードqueryと検索対象optionの条件で検索を行う。 入力値：query"ノーネイム", option"author"
+     * 期待する結果：null
+     * 
+     * 内容：DBから受け取ったリストが空っぽの場合はnull。
+     */
+    @Test
+    void testPartialMatchAuthor_dbEmpty() {
+        List<Quote> allList = new ArrayList<>();
+
+        Mockito.when(quoteRepositoryMock.findAll()).thenReturn(allList);
+
+        assertNull(injectQuoteService.partialMatch("ノーネイム", "author"));
+
+    }
+
+    /*
+     * 前提条件：DBから名言オブジェクトを取得 手順：キーワードqueryと検索対象optionの条件で検索を行う。 入力値：query"ン", option"author"
+     * 期待する結果："地球は青かった", "ガガーリン" "天才は1%のひらめきと99%の努力でつくられる", "トーマス・エジソン"
+     * 
+     * 内容：ループの１回目と２回目のリスト内の要素位置を確認し、ループの開始地点と 終了条件に誤りがないか。
+     */
+    @Test
+    void testPartialMatchAuthor_roop() {
+        List<Quote> allList = new ArrayList<>();
+        List<Quote> actuaList = new ArrayList<>();
+        List<Quote> expectedList = new ArrayList<>();
+        allList.add(new Quote("地球は青かった", "ガガーリン"));
+        allList.add(new Quote("天才は1%のひらめきと99%の努力でつくられる", "トーマス・エジソン"));
+        allList.add(new Quote("憧れるのをやめましょう", "大谷翔平"));
+        allList.add((new Quote("僕の大冒険", "僕")));
+
+        Mockito.when(quoteRepositoryMock.findAll()).thenReturn(allList);
+
+        actuaList.addAll(injectQuoteService.partialMatch("ン", "author"));
+        expectedList.add(allList.get(0));
+        expectedList.add(allList.get(1));
+        assertIterableEquals(expectedList, actuaList);
+    }
+
+    /*
+     * 前提条件：DBから名言オブジェクトを取得 手順：キーワードqueryと検索対象optionの条件で検索を行う。 入力値：query"僕"、option"both"
+     * 期待する結果：text"僕の大冒険", author"僕"
+     * 
+     * 内容：検索対象が両方の場合、部分一致した名言オブジェクトの リストを作成し、そのリストを取り出せるかどうか。名言と著者に同じ
+     * キーワードがあった際に、リスト内に同じ名言オブジェクトないか。
+     */
+    @Test
+    void testPartialMatchBoth_main() {
+        List<Quote> allList = new ArrayList<>();
+        List<Quote> actuaList = new ArrayList<>();
+        List<Quote> expectedList = new ArrayList<>();
+        allList.add(new Quote("地球は青かった", "ガガーリン"));
+        allList.add(new Quote("天才は1%のひらめきと99%の努力でつくられる", "トーマス・エジソン"));
+        allList.add(new Quote("憧れるのをやめましょう", "大谷翔平"));
+        allList.add((new Quote("僕の大冒険", "僕")));
+
+        Mockito.when(quoteRepositoryMock.findAll()).thenReturn(allList);
+
+        actuaList.addAll(injectQuoteService.partialMatch("僕", "both"));
+        expectedList.add(allList.get(3));
+        assertIterableEquals(expectedList, actuaList);
+
+    }
+
+    /*
+     * 前提条件：DBから名言オブジェクトを取得 手順：キーワードqueryと検索対象optionの条件で検索を行う。 入力値：query"両方", option"both"
+     * 期待する結果：null
+     * 
+     * 内容：検索対象が両方の場合、部分一致した名言オブジェクトの リストが空っぽの際にnull。
+     */
+    @Test
+    void testPartialMatchBoth_quoteEmpty() {
+        List<Quote> allList = new ArrayList<>();
+        allList.add(new Quote("地球は青かった", "ガガーリン"));
+        allList.add(new Quote("天才は1%のひらめきと99%の努力でつくられる", "トーマス・エジソン"));
+        allList.add(new Quote("憧れるのをやめましょう", "大谷翔平"));
+        allList.add((new Quote("僕の大冒険", "僕")));
+
+        Mockito.when(quoteRepositoryMock.findAll()).thenReturn(allList);
+
+        assertNull(injectQuoteService.partialMatch("両方", "both"));
+
+    }
+
+    /*
+     * 前提条件：DBから名言オブジェクトを取得 手順：キーワードqueryと検索対象optionの条件で検索を行う。 入力値：query"両方", option"both"
+     * 期待する結果：null
+     * 
+     * 内容：DBから受け取ったリストが空っぽの場合はnull。
+     */
+    @Test
+    void testPartialMatchBoth_dbEmpty() {
+        List<Quote> allList = new ArrayList<>();
+
+        Mockito.when(quoteRepositoryMock.findAll()).thenReturn(allList);
+
+        assertNull(injectQuoteService.partialMatch("両方", "both"));
+
+    }
+
+    /*
+     * 前提条件：DBから名言オブジェクトを取得 手順：キーワードqueryと検索対象optionの条件で検索を行う。 入力値：query"僕", option"both"
+     * 期待する結果："僕のお家", "僕家" "僕の大冒険", "僕"
+     * 
+     * 内容：ループの１個目と２個目のリスト内の要素位置を確認し、ループの開始地点と 終了条件に誤りがないか。
+     */
+    @Test
+    void testPartialMatchBoth_roop() {
+        List<Quote> allList = new ArrayList<>();
+        List<Quote> actuaList = new ArrayList<>();
+        List<Quote> expectedList = new ArrayList<>();
+        allList.add(new Quote("僕のお家", "僕家"));
+        allList.add(new Quote("地球は青かった", "ガガーリン"));
+        allList.add(new Quote("天才は1%のひらめきと99%の努力でつくられる", "トーマス・エジソン"));
+        allList.add(new Quote("憧れるのをやめましょう", "大谷翔平"));
+        allList.add((new Quote("僕の大冒険", "僕")));
+
+        Mockito.when(quoteRepositoryMock.findAll()).thenReturn(allList);
+
+        actuaList.addAll(injectQuoteService.partialMatch("僕", "both"));
+        expectedList.add(allList.get(0));
+        expectedList.add(allList.get(4));
+        assertIterableEquals(expectedList, actuaList);
+    }
+
+    /*
+     * 前提条件：DBから名言オブジェクトを取得 手順：キーワードqueryと検索対象optionの条件で検索を行う。 入力値：query"我", option"both"
+     * 期待する結果："僕のお家", "我家"
+     * 
+     * 内容：著者分岐の場合にループ１個目の処理が適切に行われるか。
+     */
+    @Test
+    void testPartialMatchBoth_roopAuthorOne() {
+        List<Quote> allList = new ArrayList<>();
+        List<Quote> actuaList = new ArrayList<>();
+        List<Quote> expectedList = new ArrayList<>();
+        allList.add(new Quote("僕のお家", "我家"));
+        allList.add(new Quote("地球は青かった", "ガガーリン"));
+        allList.add(new Quote("天才は1%のひらめきと99%の努力でつくられる", "トーマス・エジソン"));
+        allList.add(new Quote("憧れるのをやめましょう", "大谷翔平"));
+        allList.add((new Quote("僕の大冒険", "僕")));
+
+        Mockito.when(quoteRepositoryMock.findAll()).thenReturn(allList);
+
+        actuaList.addAll(injectQuoteService.partialMatch("我", "both"));
+        expectedList.add(allList.get(0));
+        assertIterableEquals(expectedList, actuaList);
+    }
+
+    /*
+     * 前提条件：DBから名言オブジェクトを取得 手順：キーワードqueryと検索対象optionの条件で検索を行う。 入力値：query"ン", option"both"
+     * 期待する結果："地球は青かった", "ガガーリン" "天才は1%のひらめきと99%の努力でつくられる","トーマス・エジソン"
+     * 
+     * 内容：著者分岐の場合にループ2個目以降の処理が適切に行われるか。
+     */
+    @Test
+    void testPartialMatchBoth_roopAuthorTwo() {
+        List<Quote> allList = new ArrayList<>();
+        List<Quote> actuaList = new ArrayList<>();
+        List<Quote> expectedList = new ArrayList<>();
+        allList.add(new Quote("僕のお家", "僕家"));
+        allList.add(new Quote("地球は青かった", "ガガーリン"));
+        allList.add(new Quote("天才は1%のひらめきと99%の努力でつくられる", "トーマス・エジソン"));
+        allList.add(new Quote("憧れるのをやめましょう", "大谷翔平"));
+        allList.add((new Quote("僕の大冒険", "僕")));
+
+        Mockito.when(quoteRepositoryMock.findAll()).thenReturn(allList);
+
+        actuaList.addAll(injectQuoteService.partialMatch("ン", "both"));
+        expectedList.add(allList.get(1));
+        expectedList.add(allList.get(2));
+        assertIterableEquals(expectedList, actuaList);
+    }
+
+    /*
+     * 例外発生
+     */
+    @Test
+    void testStackTrace() {
+        Mockito.when(quoteRepositoryMock.findAll()).thenThrow(new RuntimeException());
+        List<Quote> method = injectQuoteService.partialMatch("僕", "text");
+        assertNull(method);
+
+    }
 }
